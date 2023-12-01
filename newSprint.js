c7c53c7d606a9b4cfb9ebd9b4d8863b48070b79a
MaloGermond <github@matchalo.org>
2023-12-01 13:00:27 +0100

    [feat] Add page generation template script

---
+++ newSprint.js
@@ -0,0 +1,45 @@
+const frame = figma.createFrame();
+frame.resize(3330, 1520);
+frame.x = 118
+frame.y = 118
+frame.backgrounds = [{ type: 'SOLID', color: { r: 0.251, g: 0.251, b: 0.251} }] 
+frame.name = "section"
+
+
+const page = figma.createPage()
+page.name = "↪  Design v0.1 (Sprint NN)"
+page.appendChild(frame)
+
+let cGap = 118
+let column = 3330
+let columnNb = 7
+
+let rGap =  118
+let row = 1520
+let rowNb = 20
+
+let guides = Array()
+
+for (let i = 0 ; i <= (column+cGap)*columnNb; i+=(column+cGap)){
+    guides.push({
+        axis: 'X', offset: i
+    })
+    guides.push({
+        axis: 'X', offset: i+cGap
+    })
+}
+
+for (let i = 0 ; i <= (row+rGap)*rowNb; i+=(row+rGap)){
+    guides.push({
+        axis: 'Y', offset: i
+    })
+    guides.push({
+        axis: 'Y', offset: i+rGap
+    })
+}
+
+
+// Add guides to the frame
+const horizontalGuide = { axis: 'Y', offset: 0 }; // 'Y' for horizontal
+const verticalGuide = { axis: 'X', offset: 0 };   // 'X' for vertical
+page.guides = guides;
